a2931fa0cb33d4841072ffe3d52603c386dcafe0
Neel-gohqin <neelnaymar@gmail.com>
2024-11-27 23:33:11 +0530

    update 2.1

--- prtflio/ect.js
+++ prtflio/ect.js
@@ -1,34 +1,26 @@
 
-var prevScrollpos = window.scroll
+
+var prevScrollpos = window.scrollY
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navhad").style.top = "0";
+
+  if (prevScrollpos < 400) {
+    document.getElementById("navhad").style.top = "-130px";
+    document.getElementById("useruc").style.display = "none";
+    document.getElementById("navhad").style.background="black"
+    document.getElementById("navlis").style.display="flex"
+         document.getElementById("navhad").style.backdropFilter = "blur(10px)"
   } else {
-    document.getElementById("navhad").style.background="black opacity=20%";
-    document.getElementById("navhad").style.top = "-340px";
+    document.getElementById("navhad").style.top = "0px";
+    document.getElementById("navhad").style.backdropFilter = "blur(12px)"
+    document.getElementById("navlis").style.display="none"
+    document.getElementById("navhad").style.background="transparent";
+    document.getElementById("useruc").style.display = "block";
   }
   prevScrollpos = currentScrollPos;
 
 }
 
-window.addEventListener('scroll',()=>{
-  let scrolled = window.scrollY;
-  let sdb = document.getElementById("sdebox")
-
-  if (scrolled > 320) {
-    sdb.style.display = 'block';
-
-    setTimeout(() => {
-      sdb.style.height = '66vh'; 
-    }, 10);
-  } else {
-    sdb.style.height = '0vh'; 
-    setTimeout(() => {
-      sdb.style.display = 'none'; 
-    }, 500); 
-  }
-});
 
 
 
